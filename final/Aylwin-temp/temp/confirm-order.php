1a4605e108a06e5e7441722041cbb674dc72b2cd
Aylwin <aylwinscw@gmail.com>
2018-05-08 23:16:37 +0800

    Apply Discount with promotion code feature added

--- final/Aylwin-temp/temp/confirm-order.php
+++ final/Aylwin-temp/temp/confirm-order.php
@@ -1,17 +1,37 @@
 <?php
 include 'includes/connect.php';
 include 'includes/wallet.php';
+$continue=0;
 $total = 0;
-	if($_SESSION['customer_sid']==session_id())
-	{
+if($_SESSION['customer_sid']==session_id())
+{
+		if($_POST['payment_type'] == 'Wallet'){
+		$_POST['cc_number'] = str_replace('-', '', $_POST['cc_number']);
+		$_POST['cc_number'] = str_replace(' ', '', $_POST['cc_number']); 
+		$_POST['cvv_number'] = (int)str_replace('-', '', $_POST['cvv_number']);
+		$sql1 = mysqli_query($con, "SELECT * FROM wallet_details where wallet_id = $wallet_id");
+		while($row1 = mysqli_fetch_array($sql1)){
+			$card = $row1['number'];
+			$cvv = $row1['cvv'];
+			if($card == $_POST['cc_number'] && $cvv==$_POST['cvv_number'])
+			$continue=1;
+			else
+				header("location:index.php");
+		}
+		}
+		else
+			$continue=1;
+}
+
 $result = mysqli_query($con, "SELECT * FROM users where id = $user_id");
 while($row = mysqli_fetch_array($result)){
-$name = $row['name'];	
-$address = $row['address'];
-$contact = $row['contact'];
-$verified = $row['verified'];
+	$name = $row['name'];
+	$contact = $row['contact'];
 }
-		?>
+
+if($continue){
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -38,50 +58,8 @@ $verified = $row['verified'];
   <!-- Custome CSS-->    
   <link href="css/custom/custom.min.css" type="text/css" rel="stylesheet" media="screen,projection">
 
-  <!-- INCLUDED PLUGIN CSS ON THIS PAGE -->
   <link href="js/plugins/perfect-scrollbar/perfect-scrollbar.css" type="text/css" rel="stylesheet" media="screen,projection">
-       <style type="text/css">
-  .input-field div.error{
-    position: relative;
-    top: -1rem;
-    left: 0rem;
-    font-size: 0.8rem;
-    color:#FF4081;
-    -webkit-transform: translateY(0%);
-    -ms-transform: translateY(0%);
-    -o-transform: translateY(0%);
-    transform: translateY(0%);
-  }
-  .input-field label.active{
-      width:100%;
-  }
-  .left-alert input[type=text] + label:after, 
-  .left-alert input[type=password] + label:after, 
-  .left-alert input[type=email] + label:after, 
-  .left-alert input[type=url] + label:after, 
-  .left-alert input[type=time] + label:after,
-  .left-alert input[type=date] + label:after, 
-  .left-alert input[type=datetime-local] + label:after, 
-  .left-alert input[type=tel] + label:after, 
-  .left-alert input[type=number] + label:after, 
-  .left-alert input[type=search] + label:after, 
-  .left-alert textarea.materialize-textarea + label:after{
-      left:0px;
-  }
-  .right-alert input[type=text] + label:after, 
-  .right-alert input[type=password] + label:after, 
-  .right-alert input[type=email] + label:after, 
-  .right-alert input[type=url] + label:after, 
-  .right-alert input[type=time] + label:after,
-  .right-alert input[type=date] + label:after, 
-  .right-alert input[type=datetime-local] + label:after, 
-  .right-alert input[type=tel] + label:after, 
-  .right-alert input[type=number] + label:after, 
-  .right-alert input[type=search] + label:after, 
-  .right-alert textarea.materialize-textarea + label:after{
-      right:70px;
-  }
-  </style>
+
 </head>
 
 <body>
@@ -107,7 +85,7 @@ $verified = $row['verified'];
                     <ul class="right hide-on-med-and-down">                        
                         <li><a href="#" class="waves-effect waves-block waves-light"><i class="mdi-editor-attach-money"><?php echo $balance;?></i></a>
                         </li>
-                    </ul>						
+                    </ul>					
                 </div>
             </nav>
         </div>
@@ -208,89 +186,8 @@ $verified = $row['verified'];
 
 
         <!--start container-->
-				<div class="container">
-          <p class="caption">Provide required delivery and payment details.</p>
-          <div class="divider"></div>
-            <div class="row">
-              <div class="col s12 m4 l3">
-                <h4 class="header">Details</h4>
-              </div>
-<div>
-                <div class="card-panel">
-                  <div class="row">
-                    <form class="formValidate col s12 m12 l6" id="formValidate" method="post" action="confirm-order.php" novalidate="novalidate">
-                      <div class="row">
-                        <div class="input-field col s12">
-							<label for="payment_type">Payment Type</label><br><br>
-							<select id="payment_type" name="payment_type">
-									<option value="Wallet" selected>Wallet</option>
-									<option value="Cash On Delivery" <?php if(!$verified) echo 'disabled';?>>Cash on Delivery</option>							
-							</select>
-                        </div>
-                      </div>					
-                      <div class="row">
-                        <div class="input-field col s12">
-                          <i class="mdi-action-home prefix"></i>
-							<textarea name="address" id="address" class="materialize-textarea validate" data-error=".errorTxt1"><?php echo $address;?></textarea>
-							<label for="address" class="">Address</label>
-							<div class="errorTxt1"></div>
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="input-field col s12">
-                          <i class="mdi-action-credit-card prefix"></i>
-							<input name="cc_number" id="cc_number" type="text" data-error=".errorTxt2" required>
-							<label for="cc_number" class="">Card Number</label>
-							<div class="errorTxt2"></div>
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="input-field col s12">
-                          <i class="mdi-communication-vpn-key prefix"></i>	
-							<input name="cvv_number" id="cvv_number" type="text" data-error=".errorTxt3" required>
-							<label for="cvv_number" class="">CVV Number</label>								
-							<div class="errorTxt3"></div>
-                        </div>
-                      </div>
-                      <div class="row">
-                        <div class="input-field col s12">
-                          <i class="mdi-editor-insert-emoticon prefix"></i>  
-              <input name="promo_code" id="promo_code" type="text" data-error=".errorTxt4">
-              <label for="promo_code" class="">Promo code (If any)</label>               
-              <div class="errorTxt4"></div>
-                        </div>
-                      </div>      					  
-                      <div class="row">
-                        <div class="row">
-                          <div class="input-field col s12">
-                            <button class="btn cyan waves-effect waves-light right" type="submit" name="action">Submit
-                              <i class="mdi-content-send right"></i>
-                            </button>
-                          </div>
-                        </div>
-                      </div>
-					  <?php
-					  	foreach ($_POST as $key => $value)
-						{
-							if($key == 'action' || $value == ''){
-								break;
-							}
-							echo '<input name="'.$key.'" type="hidden" value="'.$value.'">';
-						}
-					  ?>
-                    </form>
-                  </div>
-                </div>
-              </div>
-            <div class="divider"></div>
-            
-          </div>
-        <!--end container-->
-
-      </div>
-	  
         <div class="container">
-          <p class="caption">Estimated Receipt</p>
+          <p class="caption">Receipt</p>
           <div class="divider"></div>
           <!--editableTable-->
 <div id="work-collections" class="seaction">
@@ -302,14 +199,13 @@ $verified = $row['verified'];
         <i class="mdi-content-content-paste red circle"></i>
         <p><strong>Name:</strong>'.$name.'</p>
 		<p><strong>Contact Number:</strong> '.$contact.'</p>
+		<p><strong>Address:</strong> '.htmlspecialchars($_POST['address']).'</p>	
+		<p><strong>Payment Type:</strong> '.$_POST['payment_type'].'</p>			
         <a href="#" class="secondary-content"><i class="mdi-action-grade"></i></a>';
 		
 	foreach ($_POST as $key => $value)
 	{
-		if($value == ''){
-			break;
-		}
-		if(is_numeric($key)){
+		if(is_numeric($key)){		
 		$result = mysqli_query($con, "SELECT * FROM food_item WHERE id = $key");
 		while($row = mysqli_fetch_array($result))
 		{
@@ -330,15 +226,27 @@ $verified = $row['verified'];
                 <span>RM '.$price.'</span>
             </div>
         </div>
-    </li>
+    </li>';
 
-    ';
-
-    $total = $total + $price;
+		$total = $total + $price;
 	}
 	}
-    echo '
-    <li class="collection-item">
+    if ($_POST['promo_code'] == 'HELLO') {
+
+    /** added discount by Aylwin **/
+    $total = $total * 0.8;
+    echo '<li class="collection-item">
+        <div class="row">
+            <div class="col s7">
+                <p class="collections-title"> Discount</p>
+            </div>
+            <div class="col s2">
+                <span>&nbsp;</span>
+            </div>
+            <div class="col s3">
+                <span><strong>-20%</strong></span>
+            </div>
+        </div>
         <div class="row">
             <div class="col s7">
                 <p class="collections-title"> Total</p>
@@ -347,14 +255,60 @@ $verified = $row['verified'];
                 <span>&nbsp;</span>
             </div>
             <div class="col s3">
-                <span id="displayTotal"><strong>RM '.$total.'</strong></span>
+                <span><strong>RM '.$total.'</strong></span>
             </div>
         </div>
-    </li>
-    ';
-		if(!empty($_POST['description']))
+    </li>'; } else {
+      echo '<li class="collection-item">
+        <div class="row">
+            <div class="col s7">
+                <p class="collections-title"> Total</p>
+            </div>
+            <div class="col s2">
+                <span>&nbsp;</span>
+            </div>
+            <div class="col s3">
+                <span><strong>RM '.$total.'</strong></span>
+            </div>
+        </div>
+    </li>';
+    }
+	if(!empty($_POST['description']))
 		echo '<li class="collection-item avatar"><p><strong>Note: </strong>'.htmlspecialchars($_POST['description']).'</p></li>';
+	if($_POST['payment_type'] == 'Wallet')
+	echo '<div id="basic-collections" class="section">
+		<div class="row">
+			<div class="collection">
+				<a href="#" class="collection-item">
+					<div class="row"><div class="col s7">Current Balance</div><div class="col s3">'.$balance.'</div></div>
+				</a>
+				<a href="#" class="collection-item active">
+					<div class="row"><div class="col s7">Balance after purchase</div><div class="col s3">'.($balance-$total).'</div></div>
+				</a>
+			</div>
+		</div>
+	</div>';
 ?>
+<form action="routers/order-router.php" method="post">
+<?php
+foreach ($_POST as $key => $value)
+{
+	if(is_numeric($key)){
+		echo '<input type="hidden" name="'.$key.'" value="'.$value.'">';
+	}
+}
+?>
+<input type="hidden" name="payment_type" value="<?php echo $_POST['payment_type'];?>">
+<input type="hidden" name="address" value="<?php echo htmlspecialchars($_POST['address']);?>">
+<?php if (isset($_POST['description'])) { echo'<input type="hidden" name="description" value="'.htmlspecialchars($_POST['description']).'">';}?>
+<?php if($_POST['payment_type'] == 'Wallet') echo '<input type="hidden" name="balance" value="<?php echo ($balance-$total);?>">'; ?>
+<input type="hidden" name="total" value="<?php echo $total;?>">
+<div class="input-field col s12">
+<button class="btn cyan waves-effect waves-light right" type="submit" name="action" <?php if($_POST['payment_type'] == 'Wallet') {if ($balance-$total < 0) {echo 'disabled'; }}?>>Confirm Order
+<i class="mdi-content-send right"></i>
+</button>
+</div>
+</form>
 </ul>
 
 
@@ -402,73 +356,11 @@ $verified = $row['verified'];
     <!--materialize js-->
     <script type="text/javascript" src="js/materialize.min.js"></script>
     <!--scrollbar-->
-    <script type="text/javascript" src="js/plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script>
-    <script type="text/javascript" src="js/plugins/jquery-validation/jquery.validate.min.js"></script>
-    <script type="text/javascript" src="js/plugins/jquery-validation/additional-methods.min.js"></script>	
-	<script type="text/javascript" src="js/plugins/formatter/jquery.formatter.min.js"></script>   
+    <script type="text/javascript" src="js/plugins/perfect-scrollbar/perfect-scrollbar.min.js"></script> 
     <!--plugins.js - Some Specific JS codes for Plugin Settings-->
     <script type="text/javascript" src="js/plugins.min.js"></script>
     <!--custom-script.js - Add your own theme custom JS-->
     <script type="text/javascript" src="js/custom-script.js"></script>
-	<script type="text/javascript">
-    $("#formValidate").validate({
-        rules: {
-            address: {
-                required: true,
-                minlength: 5
-            },
-            cc_number: {
-                required: true,
-                minlength: 16,
-            },
-            cvv_number: {
-                required: true,
-                minlength: 3,
-			},
-		},
-        messages: {
-           address:{
-                required: "Enter a address",
-                minlength: "Enter at least 5 characters"
-            },	
-           cc_number:{
-                required: "Please provide card number",
-                minlength: "Enter at least 16 digits",
-            },	
-           cvv_number:{
-                required: "Please provide CVV number",
-                minlength: "Enter at least 3 digits",		
-            },				
-		},
-        errorElement : 'div',
-        errorPlacement: function(error, element) {
-          var placement = $(element).data('error');
-          if (placement) {
-            $(placement).append(error)
-          } else {
-            error.insertAfter(element);
-          }
-        }
-     });
-	  $('#cc_number').formatter({
-          'pattern': '{{9999}}-{{9999}}-{{9999}}-{{9999}}',
-          'persistent': true
-      });
-	  $('#cvv_number').formatter({
-          'pattern': '{{9}}-{{9}}-{{9}}',
-          'persistent': true
-      });
-		$('#payment_type').change(function() {
-		if ($(this).val() === 'Cash On Delivery') {
-		  $("#cc_number").prop('disabled', true);
-		  $("#cvv_number").prop('disabled', true);		  
-		}
-		if ($(this).val() === 'Wallet'){
-		  $("#cc_number").prop('disabled', false);
-		  $("#cvv_number").prop('disabled', false);	
-		}
-		});
-    </script>
 </body>
 
 </html>
